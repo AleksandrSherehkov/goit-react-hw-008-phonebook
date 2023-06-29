7a5087cc6dfece78345db29dfac5cbd87176ceec
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-06-29 22:46:49 +0300

    add final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 import { GlobalStyle } from 'utilities/styles/GlobalStyle';
 import { Section } from 'components/Section/Section';
 import { Report } from 'notiflix/build/notiflix-report-aio';
@@ -8,92 +8,64 @@ import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Message } from 'components/Message/Message';
 import contactsList from 'data/contactsList.json';
-import { save, load } from 'utilities/localStorage';
+
+import { useLocalStorage } from 'hooks/useLocalStorage';
 
 const KEY_LOCAL_STORAGE = 'Phonebook-contacs';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage(KEY_LOCAL_STORAGE, contactsList);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const lsContacts = load(KEY_LOCAL_STORAGE);
-    const saveContact = lsContacts || contactsList;
-
-    this.setState({ contacts: saveContact });
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-
-    if (contacts !== prevState.contacts) {
-      save(KEY_LOCAL_STORAGE, contacts);
-    }
-  }
-
-  createContact = data => {
+  const createContact = data => {
     const newContact = {
       id: nanoid(),
       ...data,
     };
-    this.checkForDuplicate(newContact);
+    checkForDuplicate(newContact);
   };
 
-  checkForDuplicate = newContact => {
-    const { contacts } = this.state;
+  const checkForDuplicate = newContact => {
     contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
-      ? this.onReportWarning(newContact)
-      : this.addContact(newContact);
+      ? onReportWarning(newContact)
+      : addContact(newContact);
   };
 
-  addContact = newContact => {
-    this.setState(({ contacts }) => ({
-      contacts: [newContact, ...contacts],
-    }));
+  const addContact = newContact => {
+    setContacts(prev => [newContact, ...prev]);
   };
 
-  onReportWarning = ({ name }) => {
+  const onReportWarning = ({ name }) => {
     Report.warning(`${name}`, 'This user is already in the contact list.', 'OK');
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prev => prev.filter(contact => contact.id !== contactId));
   };
 
-  changeFilter = e => {
-    const input = e.currentTarget;
-    this.setState({ filter: input.value });
+  const changeFilter = ({ target: { value } }) => {
+    setFilter(value);
   };
 
-  filtredContacts = () => {
-    const { filter, contacts } = this.state;
+  const filtredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(({ name }) => name.toLowerCase().includes(normalizedFilter));
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const { createContact, changeFilter, deleteContact, filtredContacts } = this;
-
-    return (
-      <>
-        <Section title="Phonebook">
-          <ContactForm createContact={createContact} />
-        </Section>
-        <Section title="Contacts">
-          <Filter filter={filter} changeFilter={changeFilter} />
-          {contacts.length > 0 ? (
-            <ContactList contacts={filtredContacts()} deleteContact={deleteContact} />
-          ) : (
-            <Message text="Contact list is empty." />
-          )}
-        </Section>
-        <GlobalStyle />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm createContact={createContact} />
+      </Section>
+      <Section title="Contacts">
+        <Filter filter={filter} changeFilter={changeFilter} />
+        {contacts.length > 0 ? (
+          <ContactList contacts={filtredContacts()} deleteContact={deleteContact} />
+        ) : (
+          <Message text="Contact list is empty." />
+        )}
+      </Section>
+      <GlobalStyle />
+    </>
+  );
+};
