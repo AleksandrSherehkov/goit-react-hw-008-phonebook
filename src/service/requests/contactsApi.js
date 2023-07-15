fc30e10aeae4aea5f97aada7e91932afe873e9bc
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-07-15 14:44:27 +0300

    add

--- src/service/requests/contactsApi.js
+++ src/service/requests/contactsApi.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
+const instance = axios.create({
   baseURL: 'https://62fd26bfb9e38585cd4da3e5.mockapi.io',
 });
 
-export const getContacts = () => api.get('/contacts');
+export const getContacts = () => instance.get('/contacts');
 
-export const addContact = contact => api.post('/contacts', contact);
+export const addContact = contact => instance.post('/contacts', contact);
 
-export const deleteContact = contactId => api.delete(`/contacts/${contactId}`);
+export const deleteContact = contactId => instance.delete(`/contacts/${contactId}`);
