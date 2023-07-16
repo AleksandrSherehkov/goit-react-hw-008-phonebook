3e6e7f325e98eb7301e5795d6d6121344426c153
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-07-16 10:26:31 +0300

    AddPedingReject

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -6,6 +6,7 @@ const initialState = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  error: null,
 };
 
 const authSlice = createSlice({
@@ -15,24 +16,43 @@ const authSlice = createSlice({
   reducers: {},
   extraReducers: builder =>
     builder
-      .addCase(register.pending, (state, actions) => state)
+      .addCase(register.pending, (state, actions) => {
+        state.isLoggedIn = true;
+        state.error = null;
+      })
       .addCase(register.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(register.rejected, (state, actions) => state)
-      .addCase(logIn.pending, (state, actions) => state)
+      .addCase(register.rejected, (state, actions) => {
+        state.error = actions.payload;
+        state.isLoggedIn = false;
+      })
+      .addCase(logIn.pending, (state, actions) => {
+        state.error = null;
+      })
       .addCase(logIn.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(logIn.rejected, (state, actions) => state)
+      .addCase(logIn.rejected, (state, actions) => {
+        state.error = actions.payload;
+        state.isLoggedIn = false;
+      })
+      .addCase(logOut.pending, (state, actions) => {
+        state.isLoggedIn = true;
+        state.error = null;
+      })
       .addCase(logOut.fulfilled, state => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
+      })
+      .addCase(logOut.rejected, (state, actions) => {
+        state.error = actions.payload;
+        state.error = actions.payload.user;
       })
       .addCase(refreshUser.pending, state => {
         state.isRefreshing = true;
